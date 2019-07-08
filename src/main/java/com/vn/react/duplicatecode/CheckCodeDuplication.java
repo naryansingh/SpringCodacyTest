05b438687aac31b54a416ecc768a1c8ed3247aa3
vishab.singh <vishab.singh@tavant.com>
2019-07-08 16:33:50 +0530

    #2 Duplicate Code in CheckDuplicate Class

--- src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
+++ src/main/java/com/vn/react/duplicatecode/CheckCodeDuplication.java
@@ -357,5 +357,37 @@ public class CheckCodeDuplication {
 
     }
 
+    public void showDuplicate21() {
 
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
+
+    public void showDuplicate22() {
+
+        int sum_a = 0;
+
+        for (int i = 0; i < 4; i++)
+            sum_a += array_a[i];
+
+        int average_a = sum_a / 4;
+
+        int sum_b = 0;
+        for (int i = 0; i < 4; i++)
+            sum_b += array_b[i];
+
+        int average_b = sum_b / 4;
+
+    }
 }
